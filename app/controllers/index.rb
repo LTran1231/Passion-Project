83b968320210c8507e7eb85ab0208f08e9467737
Le Tran <LTran.1231@gmail.com>
2015-06-14 23:03:01 -0700

    add in search function but can't search for posts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,19 @@
 get '/' do
-	erb :index
+	erb :"/posts/search"
 end
 
+post '/search' do
+	@search_city = Pin.where(city: params[:city]).first
+	@search_country = Pin.where(country: params[:country]).first
+	if @search_city || @search_country 
+	redirect "/"
+else
+	@errors = "There is no post on the location you are searching."
+	erb :index
+end
+end
+
+# post '/cheers' do
+#   @sign_text = params[:cheer_name]
+#   redirect "/?sign_text=#{Mascot.sign_for(@sign_text)}"
+# end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,6 @@
 class Pin < ActiveRecord::Base
   has_many :posts
-
+  before_save { city.downcase! }
+  before_save { country.downcase! }
+  
 end
