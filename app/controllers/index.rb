8223b6c3f271c8bb927b47b7b6eb6109c057ca11
Le Tran <LTran.1231@gmail.com>
2015-06-20 09:57:39 -0700

    still working on search

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,17 +5,12 @@ end
 
 
 post '/search' do
-	p @search_city = City.where(city: params[:city].downcase).first
-	p @search_country = Country.where(country: params[:country].downcase).first
-	if @search_city || @search_country 
-		erb :index, layout: false 
+	search_city = City.where(city: params[:city].downcase).first
+	search_country = Country.where(country: params[:country].downcase).first
+	if search_city || search_country 
+		erb :index, locals: { city: search_city, country: search_country }, layout: false
 	else
-		@error = "There is no post on the location you are searching."
-		erb :index, layout: false
+		error = "There is no post on the location you are searching."
+
 	end
 end
-
-# post '/cheers' do
-#   @sign_text = params[:cheer_name]
-#   redirect "/?sign_text=#{Mascot.sign_for(@sign_text)}"
-# end
--- public/js/application.js
+++ public/js/application.js
@@ -1,27 +1,31 @@
 $(document).ready(function() {
+	searchButtonListener();
 
-	$('.center-search-bar').on('submit', 'form.search-form', function(event) {
-	  event.preventDefault();
-	  var $target = $(event.target);
-	  $.ajax({
-	    url: $target.attr('action'),
-	    type: 'POST',
-	    data_Type: 'Json',
-	    //data: {task: {description: $target.children('input[name=task\[description\]]').val(), priority: $target.children('select').val() }, tags: $target.children('input[name=tags]').val()}
-	    data: $target.serialize(),
-	  }).done(function(response) {
-	  	console.log(response)
-	  	debugger
-	    $target.find('.list-group').replaceWith(response);
-	  }).fail(function(response){
-	  	alert("Fail");
-	  })
-	})
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
-
 // Search From
+var searchButtonListener = function(){
+	$('.container').on('submit', 'form.search-form', function(event) {
+		event.preventDefault();
+
+
+		var target = $(event.target);
+		
+		var request = $.ajax({
+			url: target.attr('action'),
+			type: target.attr('method'),
+			data_Type: 'Json',
+  		data: target.serialize(),
+		})
+		request.done(function(response) {
+			console.log(response);
+			console.log(target)
+			target.next('div').append(response);
+		})
+
+		request.fail(function(response){
+			console.log(response);
+			alert("Fail");
+		})
+	})
+}
